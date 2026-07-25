d45d5ad46921bd09bce0099d59a04e1d44a76a2a
SuxrobMamadaliyev <mamadaliyevsuhrob69@gmail.com>
2026-07-25 19:03:40 +0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const config = require('./config');
+const logger = require('./logger');
+const { buildWebhookRouter } = require('./webhook');
+const { buildApiRouter } = require('./api');
+
+function createServer(masterBot) {
+  const app = express();
+
+  app.get('/', (req, res) => {
+    res.json({ name: 'Telegram Bot Builder Platform', status: 'running' });
+  });
+
+  app.use('/webhook', buildWebhookRouter(masterBot));
+  app.use('/api', buildApiRouter());
+
+  app.use((req, res) => {
+    res.status(404).json({ error: 'Topilmadi' });
+  });
+
+  // eslint-disable-next-line no-unused-vars
+  app.use((err, req, res, next) => {
+    logger.error({ err: err.message }, 'Express xatoligi');
+    res.status(500).json({ error: 'Ichki server xatoligi' });
+  });
+
+  return app;
+}
+
+function startServer(app) {
+  return new Promise((resolve) => {
+    const server = app.listen(config.port, () => {
+      logger.info({ port: config.port }, 'HTTP server ishga tushdi');
+      resolve(server);
+    });
+  });
+}
+
+module.exports = { createServer, startServer };
