1259986d364bac95470af9d356bb560bac672976
SuxrobMamadaliyev <mamadaliyevsuhrob69@gmail.com>
2026-07-25 19:02:46 +0500

    Create logger.js

---
+++ logger.js
@@ -0,0 +1,13 @@
+const pino = require('pino');
+const config = require('./config');
+
+const logger = pino({
+  level: config.logLevel,
+  transport:
+    config.nodeEnv !== 'production'
+      ? { target: 'pino-pretty', options: { colorize: true, translateTime: 'SYS:standard' } }
+      : undefined,
+  base: undefined,
+});
+
+module.exports = logger;
