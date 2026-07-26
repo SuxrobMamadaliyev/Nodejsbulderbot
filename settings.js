2d426f6ace2f5b31c3e7ccb3358a9da8511fd5e3
SuxrobMamadaliyev <mamadaliyevsuhrob69@gmail.com>
2026-07-26 21:58:17 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -21,46 +21,18 @@ async function setSetting(key, value) {
   return value;
 }
 
-async function getReferralRequired() {
-  const value = await getSetting('referral_required', config.defaultReferralRequired);
-  return Number(value) || config.defaultReferralRequired;
+async function getRwcoinPerReferral() {
+  const value = await getSetting('rwcoin_per_referral', config.defaultRwcoinPerReferral);
+  return Number(value) || config.defaultRwcoinPerReferral;
 }
 
-async function setReferralRequired(count) {
-  return setSetting('referral_required', Number(count));
-}
-
-async function getBotsPerReferralBatch() {
-  const value = await getSetting('bots_per_batch', 1);
-  return Number(value) || 1;
-}
-
-async function getCoinsPerReferral() {
-  const value = await getSetting('coins_per_referral', config.defaultCoinsPerReferral);
-  return Number(value) || config.defaultCoinsPerReferral;
-}
-
-async function setCoinsPerReferral(amount) {
-  return setSetting('coins_per_referral', Number(amount));
-}
-
-async function getBotPriceCoins() {
-  const value = await getSetting('bot_price_coins', config.defaultBotPriceCoins);
-  return Number(value) || config.defaultBotPriceCoins;
-}
-
-async function setBotPriceCoins(amount) {
-  return setSetting('bot_price_coins', Number(amount));
+async function setRwcoinPerReferral(amount) {
+  return setSetting('rwcoin_per_referral', Number(amount));
 }
 
 module.exports = {
   getSetting,
   setSetting,
-  getReferralRequired,
-  setReferralRequired,
-  getBotsPerReferralBatch,
-  getCoinsPerReferral,
-  setCoinsPerReferral,
-  getBotPriceCoins,
-  setBotPriceCoins,
+  getRwcoinPerReferral,
+  setRwcoinPerReferral,
 };
